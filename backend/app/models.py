0d9be5981ee40329119a26297effa46208443d38
almeizaarvin <13519066@std.stei.itb.ac.id>
2025-04-27 15:05:02 +0700

    Modify BE: models

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from datetime import datetime
 from database import Base
 
-class Task(Base):
+class TaskModel(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
